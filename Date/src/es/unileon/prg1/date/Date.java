4b48525514304ef2c5e6fd7727490c0f2c8a12b4
rsearb00 <36696319+rsearb00@users.noreply.github.com>
2018-03-26 12:48:35 +0200

    Commit 26-3-18 12:45

--- Date/src/es/unileon/prg1/date/Date.java
+++ Date/src/es/unileon/prg1/date/Date.java
@@ -15,7 +15,14 @@ public class Date {
 	public int getYear(){
 		return this.year;
 	}
+	
+	public int getMonth(){
+		return this.month;
+	}
 
+	public int getDay(){
+		return this.day;
+	}
 	boolean isSameYear(Date another){
 		if ( this.year == another.getYear() ){
 			return true;
@@ -23,8 +30,83 @@ public class Date {
 		return false;
 	}
 	
+	boolean isSameMonth(Date another){
+		if ( this.month == another.getYear() ){
+			return true;
+		}
+		return false;
+	}
+
+	boolean isSameDay(Date another){
+		if ( this.day== another.getDay() ){
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isSame(Date another){
+		if (( this.day == another.getDay())&&( this.month == another.getMonth())&&( this.year == another.getYear())){
+			return true;
+		}
+		return false;
+	}
+
 	public String toString(){
 		return this.day + "/" + this.month + "/" + this.year;
+	}
+	
+	public String getMonthName(){
+		String name;
+	
+		switch(month){
+			case 1:
+				name = "Enero";
+			break;
+	
+			case 2:
+				name = "Febrero";
+			break;
+		
+			case 3:
+				name = "Marzo";
+			break;
+		
+			case 4:
+				name = "Abril";
+			break;
+		
+			case 5:
+				name = "Mayo";
+			break;
+	
+			case 6:
+				name = "Junio";
+			break;
+		
+			case 7:
+				name = "Julio";
+			break;
+		
+			case 8:
+				name = "Agosto";
+			break;
+		
+			case 9:
+				name = "Septiembre";
+			break;
+	
+			case 10:
+				name = "Octubre";
+			break;
+		
+			case 11:
+				name = "Noviembre";
+			break;
+		
+			default:
+				name = "Diciembre";
+		}
+		return name;
 	}
 
 }
--- Date/src/es/unileon/prg1/date/MainDate.java
+++ Date/src/es/unileon/prg1/date/MainDate.java
@@ -3,10 +3,15 @@ package es.unileon.prg1.date;
 public class MainDate {
 
 	public static void main(String[] args) {
-		Date today, tomorrow;
+		Date today, tomorrow, prueba1, prueba2;
 		today = new Date(20, 3, 2018);
 		tomorrow = new Date(21, 3, 2018);
 		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYear(tomorrow));
+		/*prueba1 = new Date(23, 4, 2015);
+		prueba2 = new Date(23, 4, 2015);
+
+		System.out.println(prueba1 + " isSame " + prueba2 + "? " + prueba1.isSame(prueba2));
+		*/
 	}
 
 }
